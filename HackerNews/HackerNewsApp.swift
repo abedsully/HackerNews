f518f9c30a33fc918ba21abdb9e94bcd2f2395cb
abedsully <abedsully2909@gmail.com>
2023-09-17 13:35:03 +0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Stefanus Albert Wilson on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
